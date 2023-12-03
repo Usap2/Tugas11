2e1e9057af73b3e3b40e384e96b2c2f5fa245197
Usap2 <stephe.ndharma.putra.n@students.amikom.ac.id>
2023-12-03 20:51:57 +0700

    Tugas11

---
+++ PerpustakaanAppMVC4.6.1/PerpustakaanAppMVC4.6.1/Controller/MahasiswaController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PerpustakaanAppMVC4._6._1.Model.Entity;
+using PerpustakaanAppMVC4._6._1.Model.Repository;
+using PerpustakaanAppMVC4._6._1.Model.Context;
+
+namespace PerpustakaanAppMVC4._6._1.Controller
+{
+    public class MahasiswaController
+    {
+        // deklarasi objek Respository untuk menjalankan operasi CRUD
+        private MahasiswaRepository _repository;
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Create(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Update class repository untuk mengupdate data
+                result = _repository.Update(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil diupdate !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal diupdate !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+        public int Delete(string npm)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Delete class repository untuk menghapus data
+                result = _repository.Delete(npm);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil dihapus !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal dihapus !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetByNama yang ada di dalam class repository
+                list = _repository.ReadByNama(nama);
+            }
+            return list;
+        }
+        public List<Mahasiswa> ReadAll()
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetAll yang ada di dalam class repository
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
+    
+    }
+}
---
+++ PerpustakaanAppMVC4.6.1/PerpustakaanAppMVC4.6.1/View/FrmEntryMahasiswa.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PerpustakaanAppMVC4._6._1.View
+{
+    partial class FrmEntryMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNpm = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAngkatan = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Angkatan";
+            // 
+            // txtNpm
+            // 
+            this.txtNpm.Location = new System.Drawing.Point(71, 17);
+            this.txtNpm.Name = "txtNpm";
+            this.txtNpm.Size = new System.Drawing.Size(114, 20);
+            this.txtNpm.TabIndex = 3;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(71, 43);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(346, 20);
+            this.txtNama.TabIndex = 4;
+            // 
+            // txtAngkatan
+            // 
+            this.txtAngkatan.Location = new System.Drawing.Point(71, 69);
+            this.txtAngkatan.Name = "txtAngkatan";
+            this.txtAngkatan.Size = new System.Drawing.Size(114, 20);
+            this.txtAngkatan.TabIndex = 5;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(211, 98);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(120, 23);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Location = new System.Drawing.Point(339, 98);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(120, 23);
+            this.btnSelesai.TabIndex = 7;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            this.btnSelesai.Click += new System.EventHandler(this.btnSelesai_Click);
+            // 
+            // FrmEntryMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 133);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtAngkatan);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNpm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmEntryMahasiswa";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmEntryMahasiswa";
+            this.Load += new System.EventHandler(this.FrmEntryMahasiswa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNpm;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtAngkatan;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnSelesai;
+    }
+}
